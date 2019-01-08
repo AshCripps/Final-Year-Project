cb77e3a9321711c8196e4e554ce48fc8f3c1dbfa
Ash Cripps <14062034@brookes.ac.uk>
2019-01-08 17:13:22 +0000

    testing express

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req,res){
-  res.send("Hello World!")
+  res.render('index');
+})
+
+app.post('/', function (req, res){
+  console.log.(req.body.Collection);
+  res.render('index');
 })
 
 app.listen(3000, function(){
