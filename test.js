54b8840f02797d3fd1b9d1cc5e515a9617bf5739
Ash Cripps <14062034@brookes.ac.uk>
2019-01-08 14:14:31 +0000

    Test 2 - using callbacks

--- test.js
+++ test.js
@@ -1,13 +1,34 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("collectd");
+
+
+function createConnection(cb){
+  MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("collectd");
+    cb(dbo);
+}
+
+function read(dbo, cb){
   dbo.collection("load").findOne({}, function(err, result) {
     if (err) throw err;
     console.log("Loading file");
     console.log(result.name);
-    db.close();
-  });
+    db.close;
+}
+
+function test(cb){
+  createConnection(function(err, account)){
+    if (err) return cb(err);
+    read(dbo);
+    cb();
+  }
+}
+
+test(function(err){
+  if (err){
+    console.log("it done broked", err);
+    process.exit(1);
+  }
 });
